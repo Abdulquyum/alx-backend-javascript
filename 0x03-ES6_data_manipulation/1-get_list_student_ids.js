380f33db488b345970da5f796063a843a09a1df0
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-08-20 10:35:08 +0100

    got students id's in an array with the use of map() function

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
 export default function getListStudentIds(array) {
   if (Array.isArray(array)) {
-    const ids = array.map(students => students.id);
+    const ids = array.map((students) => students.id);
     return ids;
   }
-  return []
+  return [];
 }
