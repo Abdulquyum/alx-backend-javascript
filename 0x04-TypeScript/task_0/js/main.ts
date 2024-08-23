b6f01e2175f6338a649f772d4b5a3005b70e4eb2
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-08-23 15:18:35 +0100

    Using interface to create students list

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,21 @@
+interface Students {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Students = {
+    firstName: 'Moshood',
+    lastName: 'Adewuyi',
+    age: 20,
+    location: 'Oyo',
+};
+const student2: Students = {
+    firstName: 'Azeezah',
+    lastName: 'Hammed',
+    age: 21,
+    location: 'Ibadan',
+};
+
+const studentsList = [student1, student2];
