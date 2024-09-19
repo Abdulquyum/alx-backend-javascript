e32148ab9d9e5d7ec9359e3ac92c8164bd0f2044
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-09-19 16:32:17 +0100

    run https server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const { createServer } = require('https');
+const port = 1245;
+
+app = createServer((req, res) => {
+    res.end('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is Listening on port ${port}`);
+})
+
+module.exports = app;
