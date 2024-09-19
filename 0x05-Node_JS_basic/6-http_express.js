083445bf494917a4ef8760ffc8d0b9e8d0d1858a
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-09-19 16:48:28 +0100

    run express JS server

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,11 +1,12 @@
 const { express } = require('express');
+
 const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
-    res.send('Displays Hello Holberton School!');
+  res.send('Displays Hello Holberton School!');
 });
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
