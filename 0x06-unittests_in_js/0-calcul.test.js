9cccda1b8c27445f33661725649d901f2585db0b
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-09-21 05:45:15 +0100

    round and addition function with Mocha test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,8 +1,8 @@
 const assert = require("assert");
-const sum = require("./calculateNumber");
+const calculateNumber = require("./0-calcul");
 
-describe("addittiona and round test", function() {
-  it("should return 4 if 2.1 ia added with 2.3", function() {
+describe("addittion and round test", function() {
+  it("should return 4 if 2.1 is added with 2.3", function() {
     const result = calculateNumber(2.1, 2.3);
     assert.equal(result, 4);
   });
