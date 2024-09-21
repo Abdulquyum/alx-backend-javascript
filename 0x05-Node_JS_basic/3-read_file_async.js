0e3d9d2fc7d1bd287690122c62b14c83c5c97231
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-09-21 20:02:25 +0100

    write to stdout with node

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,35 +1,34 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  try {
-    const data = fs.readFileSync(path, 'utf8');
-    const lines = data.trim().split('\n');
-    const students = lines.slice(1);
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err)
+	  throw new Error('Cannot load the database');
+  });
 
-    console.log(`Number of students: ${students.length}`);
+   const lines = data.trim().split('\n');
+   const students = lines.slice(1);
 
-    const fields = {};
+   console.log(`Number of students: ${students.length}`);
 
-    students.forEach((students) => {
-      const details = students.split(',');
-      const firstName = details[0];
-      const field = details[details.length - 1];
+   const fields = {};
 
-      if (!fields[field]) {
-        fields[field] = [];
-      }
-      fields[field].push(firstName);
-    });
+   students.forEach((students) => {
+     const details = students.split(',');
+     const firstName = details[0];
+     const field = details[details.length - 1];
 
-    for (const field in fields) {
+     if (!fields[field]) {
+       fields[field] = [];
+     }
+     fields[field].push(firstName);
+   });
+
+  for (const field in fields) {
       if (Object.prototype.hasOwnProperty.call(fields, field)) {
         const studentList = fields[field];
         console.log(`Number of students in ${field}: ${studentList.length}. List: ${studentList.join(', ')}`);
       }
     }
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
-}
 
 module.exports = countStudents;
