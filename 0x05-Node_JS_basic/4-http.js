3232173c217d3f66ea0d49e3fd3d59534a6b9255
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-09-22 16:47:16 +0100

    test

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,11 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
-  res.end('Hello Holberton!');
-  });
+if (require.main === module) {
+  const app = http.createServer((req, res) => {
+    res.end('Hello Holberton!');
+});
 
 app.listen(1245);
 
 module.exports = app;
+}
