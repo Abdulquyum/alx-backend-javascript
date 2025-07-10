5e2807274fba297c5ac5b4c14391e3a6c0b06047
Abdulquyum <ajumobiabdulquyum@gmail.com>
2025-07-10 12:45:52 +0100

    using readline for prompt

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,2 +1,11 @@
-let intro = prompt("Welocme to ALX, What is your Name? ");
-console.log("Your name is ${intro}");
+const readline = require("readline");
+
+const message = readline.createInterface({
+    output: process.stdout,
+    input: process.stdin
+});
+
+message.question("Welcome to ALX, what is your name?\n", (input) => {
+    process.stdout.write(`Your input is ${input}`);
+    message.close("This important software is now closing");
+});
